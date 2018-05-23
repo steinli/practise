9c244953fc4c4196d612830a3db96ca421b82387
steinli <gnostikos@126.com>
2018-05-23 22:56:46 +0800

    test

---
+++ src/com/youmeek/demo/ToBeOrNotToBe.java
@@ -0,0 +1,8 @@
+package com.youmeek.demo;
+
+public class ToBeOrNotToBe {
+    public static void main(String[] args){
+        System.out.println("To be Or not to be,That is a question.");
+        //
+    }
+}
